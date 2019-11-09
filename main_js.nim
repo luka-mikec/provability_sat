68216982d9f2ff8c377c8e75f9a2f458e649679f
Luka Mikec <luka.mikec1@gmail.com>
2019-11-10 00:39:54 +0100

    ilm

--- main_js.nim
+++ main_js.nim
@@ -2,19 +2,22 @@
 import dom
 import helpers
 import formula
+import sugar
+import sequtils
 # import generator # nim'js backend doesnt support lc, used extensively there
 
 import logic_il
 import logic_ilw
 import logic_ilp
+import logic_ilm
 
 include principles
 
-proc get_principles*() : type(all_principles) {.exportc.} =
-  return all_principles
+proc get_principles*() : seq[cstring] {.exportc.} =
+  return all_principles.map(str => (cstring)str)
 
 proc check*(sf : cstring, logic : cstring) {.exportc.} =
-  let f = from_infix ($sf)
+  let f = from_infix($sf, $logic == "ilm")
   let result : bool =
     if   $logic == "il":
       sattreeil  f
@@ -22,6 +25,8 @@ proc check*(sf : cstring, logic : cstring) {.exportc.} =
       sattreeilw f
     elif $logic == "ilp":
       sattreeilp f
+    elif $logic == "ilm":
+      sattreeilm f
     else:
       (alert("not implemented!"); false)
   alert(window, cstring ($f & $ (if result: " is satisfiable." else: " is not satisfiable.")  ))
